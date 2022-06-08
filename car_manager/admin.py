032ea98a3baaf7951f1cb4de2a1aee13a56d2a55
MFireIt <wojcikk.mariusz@gmail.com>
2022-06-08 19:41:35 +0200

    register Car model register CarProductionDetail model

--- car_manager/admin.py
+++ car_manager/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from car_manager.models import Car, CarProductionDetail
 # Register your models here.
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "type_of_fuel", "inssurance_expired_date"]
+
+
+admin.register(CarProductionDetail)

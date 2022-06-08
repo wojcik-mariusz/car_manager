c758ae371ade5c726d98ecf8112f98b2ad541425
MFireIt <wojcikk.mariusz@gmail.com>
2022-06-08 19:43:32 +0200

    refactor Car model into Car, CarProductionDetail models

--- car_manager/models.py
+++ car_manager/models.py
@@ -4,14 +4,19 @@ from django.db import models
 
 
 class Car(models.Model):
-    fuel_choices: List[str] = ["Petrol", "Gasoline"]
+    fuel_choices: List[str] = [("1", "Petrol"), ("2", "Gasoline")]
 
     name = models.CharField(max_length=120)
     # optional fields
     description = models.TextField(null=True, blank=True)
-    type_of_fuel = models.CharField(choices=fuel_choices, null=True, blank=True)
+    type_of_fuel = models.CharField(max_length=2, choices=fuel_choices, null=True, blank=True)
+    inssurance_expired_date = models.DateField(null=True, blank=True)
+
+
+class CarProductionDetail(models.Model):
     company = models.TextField(null=True, blank=True)
     car_model_name = models.TextField(null=True, blank=True)
     production_year = models.IntegerField(null=True, blank=True)
     vin = models.IntegerField(null=True, blank=True)
-    inssurance_expired_date = models.DateField(null=True, blank=True)
+    name = models.OneToOneField(Car, on_delete=models.CASCADE, primary_key=True)
+

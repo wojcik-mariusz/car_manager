ed6063aba14e186dc0b54841584a57efbfec4cfc
MFireIt <wojcikk.mariusz@gmail.com>
2022-06-08 22:48:53 +0200

    change one model Car into Car, CarProductionDetail models

--- car_manager/models.py
+++ car_manager/models.py
@@ -3,6 +3,13 @@ from typing import List
 from django.db import models
 
 
+class CarProductionDetail(models.Model):
+    company = models.TextField(null=True, blank=True)
+    car_model_name = models.TextField(null=True, blank=True)
+    production_year = models.IntegerField(null=True, blank=True)
+    vin = models.IntegerField(null=True, blank=True)
+
+
 class Car(models.Model):
     fuel_choices: List[str] = [("1", "Petrol"), ("2", "Gasoline")]
 
@@ -11,12 +18,4 @@ class Car(models.Model):
     description = models.TextField(null=True, blank=True)
     type_of_fuel = models.CharField(max_length=2, choices=fuel_choices, null=True, blank=True)
     inssurance_expired_date = models.DateField(null=True, blank=True)
-
-
-class CarProductionDetail(models.Model):
-    company = models.TextField(null=True, blank=True)
-    car_model_name = models.TextField(null=True, blank=True)
-    production_year = models.IntegerField(null=True, blank=True)
-    vin = models.IntegerField(null=True, blank=True)
-    name = models.OneToOneField(Car, on_delete=models.CASCADE, primary_key=True)
-
+    detail = models.OneToOneField(CarProductionDetail, on_delete=models.CASCADE, null=True, blank=True)

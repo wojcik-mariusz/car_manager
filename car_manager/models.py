6ce6cfcb5a15acbbd82d21ebb4a2908aea58ca00
MFireIt <wojcikk.mariusz@gmail.com>
2022-06-08 19:14:11 +0200

    add car model

--- car_manager/models.py
+++ car_manager/models.py
@@ -1,3 +1,17 @@
+from typing import List
+
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    fuel_choices: List[str] = ["Petrol", "Gasoline"]
+
+    name = models.CharField(max_length=120)
+    # optional fields
+    description = models.TextField(null=True, blank=True)
+    type_of_fuel = models.CharField(choices=fuel_choices, null=True, blank=True)
+    company = models.TextField(null=True, blank=True)
+    car_model_name = models.TextField(null=True, blank=True)
+    production_year = models.IntegerField(null=True, blank=True)
+    vin = models.IntegerField(null=True, blank=True)
+    inssurance_expired_date = models.DateField(null=True, blank=True)

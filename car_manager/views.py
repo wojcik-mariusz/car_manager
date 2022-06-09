066c6ed1255143e764cb13c38d54b92c67d549c9
MFireIt <wojcikk.mariusz@gmail.com>
2022-06-09 13:27:49 +0200

    add cars queryset to context

--- car_manager/views.py
+++ car_manager/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
+
+from car_manager.services.db_services import get_all_car_obj_from_db
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    cars = get_all_car_obj_from_db()
+
+    context = {
+        "cars": cars
+    }
+
+    return render(request, 'home.html', context)
+
+
+# TODO CRUD
+def new_car(request):
+    pass
+

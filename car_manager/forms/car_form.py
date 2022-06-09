b2c273a6d8d47f25608e3ba9d954507a05443bed
Mariusz W <wojcikk.mariusz@gmail.com>
2022-06-09 16:23:18 +0200

    add detail field to CarForm

--- car_manager/forms/car_form.py
+++ car_manager/forms/car_form.py
@@ -6,9 +6,10 @@ from car_manager.models import Car, CarProductionDetail
 class CarForm(ModelForm):
     class Meta:
         model = Car
-        fields = ["name", "description", "type_of_fuel", "inssurance_expired_date", "detail"]
+        fields = ["name", "description", "type_of_fuel", "inssurance_expired_date", "user_name"]
 
 
 class CarProductionDetailForm(ModelForm):
     class Meta:
+        model = CarProductionDetail
         fields = ["company", "car_model_name", "production_year", "vin"]

e57eb5e53c38d18d21e743be15122dc99af40bca
Mariusz W <wojcikk.mariusz@gmail.com>
2022-06-12 23:29:01 +0200

    add form with email field

--- car_manager/users/views.py
+++ car_manager/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from users.forms import UserRegistrationForm
 from django.contrib import messages
 
 # Create your views here.
@@ -7,14 +7,14 @@ from django.contrib import messages
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             username: str = form.cleaned_data.get('username')
             messages.success(request, f"Account's been created for {username}")
             form.save()
             return redirect('user-home')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
 
     return render(request, "users/register.html", {"form": form})
 

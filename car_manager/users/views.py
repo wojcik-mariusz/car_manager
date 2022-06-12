115744eb77a0d317dbc537ac20743c3f7ba41522
Mariusz W <wojcikk.mariusz@gmail.com>
2022-06-12 22:24:14 +0200

    add flash message on register view

--- car_manager/users/views.py
+++ car_manager/users/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account's been created for {username}")
+            return redirect('user-home')
+    else:
+        form = UserCreationForm()
 
-    return render(request, "register.html", {"form": form})
+    return render(request, "users/register.html", {"form": form})
+
+
+def home(request):
+    return render(request, 'user-home.html')

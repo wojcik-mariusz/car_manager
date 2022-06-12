95ad2a39c453cb694e46166ce59a1bfc418a9b8d
Mariusz W <wojcikk.mariusz@gmail.com>
2022-06-12 22:26:25 +0200

    refactor/ add path_name to path cars

--- car_manager/my_car_manager/urls.py
+++ car_manager/my_car_manager/urls.py
@@ -19,7 +19,8 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', include('car.urls')),
+    path('cars/', include('car.urls'), name='car-home'),
     path('register/', user_views.register, name='register'),
+    path('register/home/', user_views.home, name='user-home'),
 
 ]

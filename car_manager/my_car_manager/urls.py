60af6934d08c2b833762d43b8eba1d0fa11bf3d1
MFireIt <wojcikk.mariusz@gmail.com>
2022-06-13 10:37:32 +0200

    add path for user profile view

--- car_manager/my_car_manager/urls.py
+++ car_manager/my_car_manager/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('register/home/', user_views.home, name='user-home'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
 
 ]

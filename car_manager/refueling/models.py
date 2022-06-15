135bc03f544619f2d1cc29adfb0b9d191b6602c1
Mariusz W <wojcikk.mariusz@gmail.com>
2022-06-16 00:02:59 +0200

    add price field

--- car_manager/refueling/models.py
+++ car_manager/refueling/models.py
@@ -9,7 +9,9 @@ from car.models import Car
 class Refueling(models.Model):
     mileage = models.IntegerField()
     date_refueling = models.DateField(default=timezone.now())
-    cost_per_litr = models.DecimalField(max_digits=4, decimal_places=2, blank=False, null=False)
+    price = models.DecimalField(max_digits=4, decimal_places=2, blank=False, null=True)
+    cost_per_litr = models.DecimalField(
+        max_digits=4, decimal_places=2, blank=False, null=False
+    )
     tanked_to_max_level = models.BooleanField(default=False)
     car = models.OneToOneField(Car, on_delete=models.CASCADE, null=True, blank=False)
-

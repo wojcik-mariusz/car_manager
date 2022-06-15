d2357ef6bbecde66ddace14c208e7d05bfec386a
MFireIt <wojcikk.mariusz@gmail.com>
2022-06-15 19:48:58 +0200

    init Refueling model fields

--- car_manager/refueling/models.py
+++ car_manager/refueling/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from datetime import datetime as dt
+
 # Create your models here.
+
+
+class Refueling(models.Model):
+    mileage = models.IntegerField()
+    date_refueling = models.DateField(default=dt.now())
+    cost_per_litr = models.DecimalField(decimal_places=2, blank=False, null=False)
+    tanked_to_max_level = models.BooleanField(default=False)
+

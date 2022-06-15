a65b335336e534bec3ac9dff9d65ebdd4106d387
Mariusz W <wojcikk.mariusz@gmail.com>
2022-06-16 00:29:47 +0200

    add ListView

--- car_manager/refueling/views.py
+++ car_manager/refueling/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from .models import Refueling
 
 # Create your views here.
+
+
+class RefuelingListView(ListView):
+    model = Refueling
+    template_name = "refuelings.html"
+    context_object_name = "refuelings"

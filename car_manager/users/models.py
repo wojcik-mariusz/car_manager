0fdfe4a3899ac33a4a208e6da0db4c570173d401
Mariusz W <wojcikk.mariusz@gmail.com>
2022-06-15 12:40:12 +0200

    fix/ wrog default profile_picture path

--- car_manager/users/models.py
+++ car_manager/users/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    picture = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    picture = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f"Profile of {self.user.username}"

d1ac94d1fbaaf22b6f590522f639a111b072074b
Mariusz W <wojcikk.mariusz@gmail.com>
2022-06-16 00:02:28 +0200

    register Refueling model

--- car_manager/refueling/admin.py
+++ car_manager/refueling/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Refueling
+
 # Register your models here.
+
+
+@admin.register(Refueling)
+class RefuelingAdmin(admin.ModelAdmin):
+    list_display = [
+        "mileage",
+        "date_refueling",
+        "price",
+        "cost_per_litr",
+        "tanked_to_max_level",
+        "car",
+    ]

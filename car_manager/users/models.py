9278e4e8de2bdf4d56afcd81863b7b453024d045
MFireIt <wojcikk.mariusz@gmail.com>
2022-06-15 19:28:40 +0200

    feature/ auto resize too big profile images

--- car_manager/users/models.py
+++ car_manager/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from PIL import Image
+
 # Create your models here.
 
 
@@ -10,3 +12,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Profile of {self.user.username}"
+
+    def save(
+        self, *args, **kwargs
+    ):
+        super().save(*args, **kwargs)
+
+        pic = Image.open(self.picture.path)
+
+        if pic.height > 300 or pic.width > 300:
+            output_size = (300, 300)
+            pic.thumbnail(output_size)
+            pic.save(self.picture.path)

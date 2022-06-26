4ef6a93d2415192941b0defc967876d2c21a03b0
kajtek <kacper.styslo@gmail.com>
2022-06-26 12:47:18 +0200

    Changed convection of endpoints paths and also of URLs name parm

--- car_manager/car/urls.py
+++ car_manager/car/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", CarListView.as_view(), name="cars_home"),
-    path("new-car/", CarCreateView.as_view(), name="add_car"),
-    path("edit-car/<int:pk>", CarUpdateView.as_view(), name="edit_car"),
-    path("detail-car/<int:pk>/", CarDetailView.as_view(), name="detail_car"),
-    path("delete-car/<int:pk>/", CarDeleteView.as_view(), name="delete_car"),
+    path("", views.CarListView.as_view(), name="cars-list"),
+    path("new/", views.CarCreateView.as_view(), name="car-add"),
+    path("edit-car/<int:pk>", views.CarUpdateView.as_view(), name="edit_car"),
+    path("detail-car/<int:pk>/", views.CarDetailView.as_view(), name="detail_car"),
+    path("<int:pk>/delete", views.CarDeleteView.as_view(), name="car-delete"),
 ]

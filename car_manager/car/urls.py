777986bc0dea59a239410c99d939553b96c8715c
Mariusz W <wojcikk.mariusz@gmail.com>
2022-06-28 20:06:14 +0200

    fix/ change url names to snake_case

--- car_manager/car/urls.py
+++ car_manager/car/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CarListView.as_view(), name="cars-list"),
-    path("new/", views.CarCreateView.as_view(), name="car-add"),
+    path("", views.CarListView.as_view(), name="cars_list"),
+    path("new/", views.CarCreateView.as_view(), name="car_add"),
     path("edit-car/<int:pk>", views.CarUpdateView.as_view(), name="edit_car"),
     path("detail-car/<int:pk>/", views.CarDetailView.as_view(), name="detail_car"),
     path("<int:pk>/delete", views.CarDeleteView.as_view(), name="car-delete"),

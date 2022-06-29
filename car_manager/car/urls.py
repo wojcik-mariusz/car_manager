b2d2644ad1946370683e8c51013b15569dc23c6c
Mariusz W <wojcikk.mariusz@gmail.com>
2022-06-29 23:21:03 +0200

    change  _ -> - in names of paths

--- car_manager/car/urls.py
+++ car_manager/car/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CarListView.as_view(), name="cars_list"),
-    path("new/", views.CarCreateView.as_view(), name="car_add"),
-    path("edit-car/<int:pk>", views.CarUpdateView.as_view(), name="edit_car"),
-    path("detail-car/<int:pk>/", views.CarDetailView.as_view(), name="detail_car"),
+    path("", views.CarListView.as_view(), name="cars-list"),
+    path("new/", views.CarCreateView.as_view(), name="car-add"),
+    path("edit-car/<int:pk>", views.CarUpdateView.as_view(), name="edit-car"),
+    path("detail-car/<int:pk>/", views.CarDetailView.as_view(), name="detail-car"),
     path("<int:pk>/delete", views.CarDeleteView.as_view(), name="car-delete"),
 ]

3d186b4d0a2cdd9cba1db293f5f955156e5ec05d
Mariusz W <wojcikk.mariusz@gmail.com>
2022-07-02 23:44:09 +0200

    finish RefuelingCreateView, works as expected

--- car_manager/refueling/views.py
+++ car_manager/refueling/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic import CreateView
+from django.shortcuts import get_object_or_404
 
-from .models import Refueling
+from refueling.models import Refueling
+from refueling.forms.refueling_form import RefuelingForm
+
+from car.models import Car
 
 # Create your views here.
 
@@ -15,4 +20,24 @@ class RefuelingListView(ListView):
 
 class RefuelingCreateView(CreateView):
     model = Refueling
-    template_name =
+    refueling_form = RefuelingForm
+    fields = "__all__"
+    template_name = "refueling-form.html"
+
+    def post(self, request, *args, **kwargs):
+        refueling_form = self.refueling_form(request.POST)
+
+        if refueling_form.is_valid():
+            refueling = refueling_form.save(commit=False)
+            refueling.car = get_object_or_404(Car, pk=self.kwargs["pk"])
+            refueling.save()
+            return redirect("all-refuelings")
+
+    def get_context_data(self, **kwargs):
+        context = super(RefuelingCreateView, self).get_context_data(**kwargs)
+        context["refueling_form"] = self.refueling_form
+        context["car"] = get_object_or_404(Car, pk=self.kwargs["pk"])
+        return context
+
+    def form_invalid(self, form):
+        return JsonResponse({"success": False})

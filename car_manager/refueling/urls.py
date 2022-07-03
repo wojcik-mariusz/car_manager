1ce5bfc9370513b168f229ef19cc156619682e58
Mariusz W <wojcikk.mariusz@gmail.com>
2022-07-03 23:25:23 +0200

    add path to DeleteView, also clean code using black lib

--- car_manager/refueling/urls.py
+++ car_manager/refueling/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 
 from django.urls import path
 
-from .views import RefuelingListView, RefuelingCreateView, RefuelingUpdateView
+from .views import (
+    RefuelingListView,
+    RefuelingCreateView,
+    RefuelingUpdateView,
+    RefuelingDeleteView,
+)
 
 urlpatterns = [
     path("", RefuelingListView.as_view(), name="all-refuelings"),
     path("new/<int:pk>/", RefuelingCreateView.as_view(), name="new-refueling"),
     path("update/<int:pk>/", RefuelingUpdateView.as_view(), name="refueling-update"),
+    path("delete/<int:pk>/", RefuelingDeleteView.as_view(), name="refueling-delete"),
 ]

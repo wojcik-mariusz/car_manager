4086caea88e12878fc0bc1e166bbe288acf5a751
Mariusz W <wojcikk.mariusz@gmail.com>
2022-07-19 21:47:05 +0200

    add path and custom template to password reset view

--- car_manager/my_car_manager/urls.py
+++ car_manager/my_car_manager/urls.py
@@ -38,5 +38,23 @@ urlpatterns = [
         name="logout",
     ),
     path("profile/", user_views.profile, name="profile"),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset.html"
+        ),
+    ),
+    # path(
+    #     "password-reset-done/",
+    #     auth_views.PasswordResetDoneView.as_view(),
+    #     name="password_reset_done"
+    # ),
+    # path("password-reset-confirm/<uidb64>/<token>/",
+    #      auth_views.PasswordResetConfirmView.as_view(),
+    #      name="password_reset_confirm"
+    #      ),
+    # path("password-reset-complete/",
+    #      auth_views.PasswordResetCompleteView.as_view(),
+    #      name="password_reset_complete"),
     path("rf/", include("refueling.urls"), name="refueling-home"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

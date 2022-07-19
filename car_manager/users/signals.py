61e28cf58f69de6904d30f9c93cda86e0cc5afce
Mariusz W <wojcikk.mariusz@gmail.com>
2022-07-19 15:45:21 +0200

    functionality to send welcome email when user is created

--- car_manager/users/signals.py
+++ car_manager/users/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.core.mail import send_mail
+
 from users.models import Profile
 
 
@@ -9,8 +11,10 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
         instance.profile.save()
-
-
-# @receiver(post_save, sender=User)
-# def save_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+        send_mail(
+            "Welcome to Car Manager",
+            "Welcome! I'm happy you're here!",
+            "",
+            [f"{instance.email}"],
+            fail_silently=False,
+        )

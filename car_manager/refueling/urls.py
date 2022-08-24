381e01d7ec157d83cc429f225c51d0d54ee66124
MFireIt <wojcikk.mariusz@gmail.com>
2022-08-24 11:07:55 +0200

    Remove links to official Django documentation.

--- car_manager/refueling/urls.py
+++ car_manager/refueling/urls.py
@@ -1,7 +1,5 @@
-"""my_car_manager URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+"""
+my_car_manager URL Configuration
 """
 
 from django.urls import path

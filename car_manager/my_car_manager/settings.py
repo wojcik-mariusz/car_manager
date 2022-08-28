fbc15f82c682090c5f148069b0efcf16b3c7ced1
Mariusz W <wojcikk.mariusz@gmail.com>
2022-08-28 09:52:50 +0200

    add aws configuration

--- car_manager/my_car_manager/settings.py
+++ car_manager/my_car_manager/settings.py
@@ -12,12 +12,12 @@ from django.core.management.utils import get_random_secret_key
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = "TAJNY_KLUCZ"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS: list[str] = ['*']
 
 # Application definition
 
